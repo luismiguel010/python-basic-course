807cc74fbd84498a8520e283b24b4cb6ad6497df
luismiguel010 <luismiguel010@gmail.com>
2021-09-07 02:22:10 -0500

    Se añade ejemplos para continue break

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+def run():
+#    for contador in range(1000):
+#       if contador % 2 != 0:
+#           continue
+#        print(contador)
+    for i in range(100):
+        print(i)
+        if i == 56:
+            break
+
+if __name__ == '__main__':
+    run()
